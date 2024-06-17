ae47176ec538b1d7b226ac4a982c4382aaed3cbf
Julian Piccoli <julianignaciopiccoli@gmail.com>
2024-06-17 20:21:24 -0300

    migrando la pagina

---
+++ cerveceria-node/routes/index.js
@@ -0,0 +1,47 @@
+var express = require('express');
+var router = express.Router();
+
+var nodemailer = require('nodemailer');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index');
+});
+
+router.post('/', async (req, res, next) =>{
+
+var nombre = req.body.nombre;
+var apellido = req.body.apellido;
+var email = req.body.email;
+var mensaje = req.body.mensaje;
+var name = req.body.check;
+
+console.log(req.body);
+
+
+var obj = {
+  to: 'julianignacio.piccoli@gmail.com',
+  subject: 'contacto desde la web',
+  html: nombre + ' ' + apellido + ' se contacto a travez de la web y quiere mas info a este correo: ' + email + ' su mensaje fue ' + mensaje
+}
+
+var transporter = nodemailer.createTransport({
+  host: process.env.SMTP_HOST,
+  port: process.env.SMTP_PORT,
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS
+  }
+})
+
+var info = await transporter.sendMail(obj);
+
+res.render('index', {
+  message: 'mensaje enviado correctamente',
+})
+
+});
+
+
+
+module.exports = router;
